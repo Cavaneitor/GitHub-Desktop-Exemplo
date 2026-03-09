a138b80a43b5df69df3891410853d48563a7b279
Matheus Cava <Matheus_cava@hotmail.com>
2026-03-09 17:38:02 -0300

    Hello World

--- GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package github.desktop.exemplo;
 
-/**
- *
- * @author Mathe
- */
-public class GitHubDesktopExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class GitHubDesktopExemplo
+{
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
     }
     
 }
